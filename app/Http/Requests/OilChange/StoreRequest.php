7fa8c8ed79ebe64c74491ef6a2f547591ee4ba7e
rodrigo-wong <wongmacr@gmail.com>
2026-01-06 00:51:21 -0500

    Add StoreRequest for validating oil change data

---
+++ app/Http/Requests/OilChange/StoreRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\OilChange;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'current_odometer' => 'required|integer|min:0|gte:prev_odometer',
+            'prev_oil_change_date' => 'required|date|before:today',
+            'prev_odometer' => 'required|integer|min:0',
+        ];
+    }
+}
