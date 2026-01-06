cf55bad39d6c8cc6d9e7a2fd9d2a21e15327e0ff
rodrigo-wong <wongmacr@gmail.com>
2026-01-06 01:35:45 -0500

    Add old input value retention and error handling for oil change form fields

--- resources/views/oil_changes/create.blade.php
+++ resources/views/oil_changes/create.blade.php
@@ -4,15 +4,24 @@
             @csrf
             <div>
                 <label for="current_odometer">Current Odometer:</label>
-                <input type="number" id="current_odometer" name="current_odometer" required>
+                <input type="number" id="current_odometer" name="current_odometer" value="{{ old('current_odometer') }}" required>
+                @error('current_odometer')
+                    <p style="color: red;">{{ $message }}</p>
+                @enderror
             </div>
             <div>
                 <label for="prev_oil_change_date">Date of Previous Oil Change:</label>
-                <input type="date" id="prev_oil_change_date" name="prev_oil_change_date" required>
+                <input type="date" id="prev_oil_change_date" name="prev_oil_change_date" value="{{ old('prev_oil_change_date') }}" required>
+                @error('prev_oil_change_date')
+                    <p style="color: red;">{{ $message }}</p>
+                @enderror   
             </div>
             <div>
                 <label for="prev_odometer">Odometer at Previous Oil Change:</label>
-                <input type="number" id="prev_odometer" name="prev_odometer" required>
+                <input type="number" id="prev_odometer" name="prev_odometer" value="{{ old('prev_odometer') }}" required>
+                @error('prev_odometer')
+                    <p style="color: red;">{{ $message }}</p>
+                @enderror
             </div>
             <button type="submit">Submit</button>
         </form>
