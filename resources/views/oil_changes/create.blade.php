0ef35897df8df1a9a305024b7fefcf72c045cc9a
rodrigo-wong <wongmacr@gmail.com>
2026-01-06 00:37:05 -0500

    Add OilChangeController and create view for oil change form

---
+++ resources/views/oil_changes/create.blade.php
@@ -0,0 +1,20 @@
+<div>
+    <div>
+        <form>
+            @csrf
+            <div>
+                <label for="current_odometer">Current Odometer:</label>
+                <input type="number" id="current_odometer" name="current_odometer" required>
+            </div>
+            <div>
+                <label for="prev_oil_change_date">Date of Previous Oil Change:</label>
+                <input type="date" id="prev_oil_change_date" name="prev_oil_change_date" required>
+            </div>
+            <div>
+                <label for="prev_odometer">Odometer at Previous Oil Change:</label>
+                <input type="number" id="prev_odometer" name="prev_odometer" required>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+</div>
